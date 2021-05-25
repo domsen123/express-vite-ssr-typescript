ed959326f197b864ba678a79f47b4633e5576010
Dominic Marx <dmarx@marxulm.e>
2021-05-25 14:42:39 +0200

    tailwind.cfg

---
+++ tailwind.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite-plugin-windicss';
+import typography from 'windicss/plugin/typography';
+
+export default defineConfig({
+  darkMode: 'class',
+  plugins: [typography()],
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: '65ch',
+            color: 'inherit',
+            a: {
+              color: 'inherit',
+              opacity: 0.75,
+              fontWeight: '500',
+              textDecoration: 'underline',
+              '&:hover': {
+                opacity: 1,
+              },
+            },
+            b: { color: 'inherit' },
+            strong: { color: 'inherit' },
+            em: { color: 'inherit' },
+            h1: { color: 'inherit' },
+            h2: { color: 'inherit' },
+            h3: { color: 'inherit' },
+            h4: { color: 'inherit' },
+            code: { color: 'inherit' },
+          },
+        },
+      },
+    },
+  },
+});
