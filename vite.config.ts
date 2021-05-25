7c3e8573b8e3a93026eeef0a5f2b87b3a973584b
Dominic Marx <dmarx@marxulm.e>
2021-05-25 14:03:03 +0200

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import Vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'app')}/`,
+    },
+  },
   plugins: [Vue()],
 });
